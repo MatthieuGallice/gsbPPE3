0f0afe66817290ca915101bedb3e5bb0fc52a860
David Jeannette <djeannette@BTS-SIO.loc>
2020-12-07 17:06:08 +0100

    méthode classe passerelle medecin

--- GSB-PPE3/ClassePasserelle/ClassePMedecin.cs
+++ GSB-PPE3/ClassePasserelle/ClassePMedecin.cs
@@ -3,10 +3,22 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Data.SqlClient;
+using ClasseMétiers;
 
 namespace ClassePasserelle
 {
     public class ClassePMedecin
     {
+        public void modifierMedecin(int idMed ,string nomMed , string prenomMed , string adresseMed , int telMed , int idSpecialisteMed , int departementMed)
+        {
+            SqlConnection connexion = new SqlConnection();
+            SqlCommand cmd = new SqlCommand();
+
+            connexion.Open();
+
+            cmd = connexion.CreateCommand();
+            cmd.CommandText = "UPDATE medecin SET nomMed ='" + nomMed + "',prenomMed ";
+        }
     }
 }
